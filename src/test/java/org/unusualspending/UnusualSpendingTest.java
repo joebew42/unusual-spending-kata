fb979e7c344aa06408781dd6e59acebf0c18ac5e
Joe Bew <joebew42@gmail.com>
2020-11-04 18:50:40 +0100

    Replace Boolean with a Trigger object

--- src/test/java/org/unusualspending/UnusualSpendingTest.java
+++ src/test/java/org/unusualspending/UnusualSpendingTest.java
@@ -2,26 +2,32 @@ package org.unusualspending;
 
 import org.junit.Test;
 
-import static java.lang.Boolean.FALSE;
 import static org.junit.Assert.assertFalse;
 
 public class UnusualSpendingTest {
     @Test
     public void do_not_trigger_the_alarm_when_the_current_amount_is_not_the_50_percent_more_of_the_previous_one() {
-        Boolean hasBeenTriggered = FALSE;
-        Alarm alarm = new SpyAlarm(hasBeenTriggered);
+        Trigger trigger = new Trigger();
+        Alarm alarm = new SpyAlarm(trigger);
 
         new UnusualSpending(alarm).evaluate(2, 2);
 
-        assertFalse(hasBeenTriggered);
+        assertFalse(trigger.hasBeenTriggered());
     }
 
     private class SpyAlarm implements Alarm {
-        private Boolean hasBeenTriggered;
+        private Trigger trigger;
 
-        public SpyAlarm(Boolean hasBeenTriggered) {
-            this.hasBeenTriggered = hasBeenTriggered;
+        public SpyAlarm(Trigger trigger) {
+            this.trigger = trigger;
         }
+    }
 
+    private class Trigger {
+        private boolean triggered = false;
+
+        public boolean hasBeenTriggered() {
+            return triggered;
+        }
     }
 }
